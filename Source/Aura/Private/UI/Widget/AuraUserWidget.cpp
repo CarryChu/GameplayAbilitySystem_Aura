03bdb47f40784ba44811284fc4ff50899b330d0d
CarryChu <1521487209@qq.com>
2025-12-25 12:24:48 +0800

    Aura User Widget and Widget Controller

---
+++ Source/Aura/Private/UI/Widget/AuraUserWidget.cpp
@@ -0,0 +1,10 @@
+// CarryChu
+
+
+#include "UI/Widget/AuraUserWidget.h"
+
+void UAuraUserWidget::SetWidgetController(UObject* InWidgetController)
+{
+	WidgetController = InWidgetController;
+	WidgetControllerSet();
+}
---
+++ Source/Aura/Private/UI/WidgetController/AuraWidgetController.cpp
@@ -0,0 +1,5 @@
+// CarryChu
+
+
+#include "UI/WidgetController/AuraWidgetController.h"
+
